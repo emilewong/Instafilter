c129f0cd4caf8935b99858c0506e317b5e444f6a
Emile Wong <emilewong@outlook.com>
2021-06-15 14:27:19 +0800

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Emile Wong on 15/6/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
